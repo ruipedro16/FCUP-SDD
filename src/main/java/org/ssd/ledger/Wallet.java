9dd25249a9191bcf18a7223dc32cefff7af8f508
Rui Fernandes <ruipedro16@protonmail.com>
2023-04-08 02:00:09 +0100

    Wallet almost finished

--- src/main/java/org/ssd/ledger/Wallet.java
+++ src/main/java/org/ssd/ledger/Wallet.java
@@ -1,16 +1,18 @@
 package org.ssd.ledger;
 
 import lombok.Data;
+import lombok.NonNull;
 import org.bouncycastle.util.encoders.Hex;
 import org.ssd.DHT;
+import org.ssd.ledger.transactions.Transaction;
+import org.ssd.ledger.transactions.TransactionInput;
 import org.ssd.ledger.transactions.TransactionOutput;
 import org.ssd.utils.CryptoUtils;
 
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /*
  * TODO: add function to create transactions
@@ -51,6 +53,59 @@ public class Wallet {
                 })
                 .sum();
     }
+
+    public Transaction createTransaction(@NonNull PublicKey recipient, double amount) {
+        // Check if the sender's balance is sufficient to make the transaction.
+        double currentBalance = getBalance();
+        if (currentBalance < amount) {
+            System.out.println("Not enough funds for creating the transaction");
+            return null;
+        }
+
+        // Find unspent transaction outputs (UTXOs) that can be used to fund the transaction.
+        List<TransactionInput> txInputs = new ArrayList<>();
+        double total = 0;
+        Iterator<TransactionOutput> utxoIterator = UTXOs.values().iterator();
+        while(utxoIterator.hasNext() && total < amount) { // stops adding inputs once the total value is greater than or equal to the transaction amount
+            TransactionOutput utxo = utxoIterator.next();
+            total += utxo.getAmount();
+            txInputs.add(new TransactionInput((utxo.getID())));
+        }
+
+        // Create a new transaction object and sign it
+        Transaction newTransaction = new Transaction(this.publicKey, recipient, amount, txInputs);
+        newTransaction.generateSignature(this.privateKey);
+
+        /*
+         * Update the UTXO lists to reflect the spent outputs and new outputs.
+         */
+        txInputs.forEach(t -> UTXOs.remove(t.getTxOutputID())); // removes the spent UTXOs from the sender's UTXO list
+
+        newTransaction.setId(Transaction.computeTransactionID(newTransaction));
+
+        // Set the new UTXOs for the resulting transaction for the recipient
+        newTransaction.getTxOutputs().add(new TransactionOutput(newTransaction.getRecipient(), amount, newTransaction.getId()));
+
+        // Set the new UTXOs for the resulting transaction for the recipient for the sender
+        double remainingAmount = newTransaction.getInputsAmount() - amount;
+        newTransaction.getTxOutputs().add(new TransactionOutput(newTransaction.getSender(), remainingAmount, newTransaction.getId()));
+
+        // Mark used inputs as spent
+        txInputs.stream()
+                .filter(t-> t.getUnspentTxOutput() != null)
+                .forEach(t -> DHT.getBlockchain().getUTXOs().remove(t.getUnspentTxOutput().getID()));
+
+        // Add new UTXOs to the blockchain
+        newTransaction.getTxOutputs().forEach(t -> DHT.getBlockchain().getUTXOs().put(t.getID(), t));
+
+        // Add the new transaction to the transaction pool.
+        DHT.getBlockchain().getTransactionPool().addTransaction(newTransaction);
+
+        // TODO: Broadcast the new transaction to other nodes in the network.
+
+        return newTransaction;
+    }
+
 
     @Override
     public String toString() {
