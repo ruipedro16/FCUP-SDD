604110b545c278d592b828f7c03872f8742ce77c
Rui Fernandes <ruipedro16@protonmail.com>
2023-04-26 03:30:22 +0100

    fix auction id

--- src/main/java/org/ssd/auction/Bid.java
+++ src/main/java/org/ssd/auction/Bid.java
@@ -1,49 +1,35 @@
 package org.ssd.auction;
 
-import com.google.protobuf.ByteString;
 import lombok.Data;
 import lombok.NonNull;
-import org.bouncycastle.util.Arrays;
-import org.bouncycastle.util.encoders.Hex;
-import org.ssd.ledger.Wallet;
-import org.ssd.utils.CryptoUtils;
-import org.ssd.utils.Utils;
 
 import java.io.Serializable;
+import java.security.PublicKey;
 
 @Data
-public class Bid implements Serializable  {
-    private final byte[] bidID;
-    private final Wallet wallet; // wallet of the bidder
-    private final Item item;
+public class Bid implements Serializable, Comparable<Bid> {
+    private final byte[] itemID;
     private final double amount;
+    private final PublicKey buyerPK;
 
-    public Bid(@NonNull Wallet wallet, @NonNull Item item, double amount) {
-        this.wallet = wallet;
-        this.item = item;
+    public Bid(byte[] itemID, double amount, @NonNull PublicKey buyerPK) {
+        if (itemID == null) {
+            throw new IllegalArgumentException();
+        }
+
+        this.itemID = itemID;
         this.amount = amount;
-        this.bidID = generateID();
-    }
-
-    private byte[] generateID() {
-        byte[] dataToHash = Arrays.concatenate(
-                this.wallet.getPublicKey().getEncoded(),
-                this.item.getItemID(),
-                Utils.toByteArray(this.amount)
-        );
-
-        return CryptoUtils.hash(dataToHash);
+        this.buyerPK = buyerPK;
     }
 
     @Override
-    public String toString() {
-        String block = """
-               Bid:
-                    Item id: %s
-                    Amount: %f
-                    Bidder: %s
-               """;
-        return String.format(block, Hex.toHexString(this.getItem().getItemID()), this.getAmount(), this.getWallet().toString());
+    public int compareTo(@NonNull Bid other) {
+        if (this.amount < other.amount) {
+            return -1;
+        } else if (this.amount > other.amount) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
-
 }
