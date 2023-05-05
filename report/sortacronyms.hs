ef9c2533dbef5865eac3f5dc61107255bc5cf491
Rui Fernandes <ruipedro16@protonmail.com>
2023-05-06 00:56:01 +0100

    report: reputation system section

--- report/sortacronyms.hs
+++ report/sortacronyms.hs
@@ -1,15 +1,23 @@
 #!/usr/bin/env runhaskell
 import Control.Arrow ((&&&))
+import Control.Monad (liftM2)
 import Data.Bifunctor (second)
 import Data.Char (toUpper)
 import Data.Function (on)
 import Data.List (nub, sortBy)
 
+capitalise :: String -> String
+capitalise = unwords . map (\(x:xs) -> toUpper x : xs) . words
+
 acronyms :: [(String, String)]
-acronyms = [("P2P", "Peer-To-Peer"), ("PoW", "Proof of Work"), ("PoS", "Proof of Stake")]
+acronyms =
+    [ ("P2P", "Peer-To-Peer")
+    , ("PoW", "Proof of Work")
+    , ("PoS", "Proof of Stake")
+    , ("RPC", "remote procedure call")
+    ]
 
 main :: IO ()
 main =
-    writeFile "acros.tex" . concat $
-    map ((\(a, s) -> concat ["\\acro{", a, "}{", s, "}\n"])) $
-    sortBy (compare `on` (map toUpper . fst)) acronyms
+    writeFile "acros.tex" . concatMap ((\(a, s) -> concat ["\\acro{", a, "}{", s, "}\n"])) $
+    sortBy (compare `on` (map toUpper . fst)) $ map (second capitalise) acronyms
